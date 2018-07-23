539f34954cb07afd05d448e62609f848ffe339e8
Reggie DePiero <rdepiero@uwyo.edu>
2018-07-22 20:48:03 -0600

    chapter 2 tests

---
+++ pythonFPM/PFMP_ch02/sec2-2-3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 22 19:26:46 2018
+
+@author: Reggie DePiero
+Math 3340, Summer 2018
+
+Description: 
+    Python for Physical Modeling Chapter 2
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+#a = np.array([2.71, 3.14, 3000])
+
+a = np.array([[2,3,5],[7,11,13] ])
+
+col = np.array([[2.71], [3.14], [3000]])
+
+row = np.array([[2.71, 3.14, 3000]])
+
+zer = np.zeros((1,2))
